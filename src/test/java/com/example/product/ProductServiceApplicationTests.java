c004e3dfd6aa12c4134c8a83dad4cc28b0beaa0e
Saurabh Kumar <saurkumar@estuate.com>
2026-01-22 12:15:55 +0530

    update changes h2 database added for temporary use

--- src/test/java/com/example/product/ProductServiceApplicationTests.java
+++ src/test/java/com/example/product/ProductServiceApplicationTests.java
@@ -2,12 +2,26 @@ package com.example.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-class ProductServiceApplicationTests {
+import com.example.product.repository.ProductRepository;
 
-	@Test
-	void contextLoads() {
+@SpringBootTest(
+	    properties = {
+	        "spring.cloud.config.enabled=false",
+	        "spring.autoconfigure.exclude=" +
+	          "org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration," +
+	          "org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration"
+	    }
+	)
+	@ActiveProfiles("test")
+	class ProductServiceApplicationTests {
+
+	    @MockBean
+	    private ProductRepository productRepository;
+
+	    @Test
+	    void contextLoads() {
+	    }
 	}
-
-}
